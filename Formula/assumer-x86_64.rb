1ab5cadc05b2da7c241c62bd636dcb335d222f6d
YAMASAKI Masahide <masahide.y@gmail.com>
2023-01-21 23:22:03 +0900

    Update assumer-x86_64.rb

--- Formula/assumer-x86_64.rb
+++ Formula/assumer-x86_64.rb
@@ -4,8 +4,8 @@
 class AssumerX8664 < Formula
   desc "AWS assume role credential wrapper"
   homepage "https://github.com/masahide/assumer"
-  url "https://github.com/masahide/assumer/releases/download/v1.0.0/assumer-1.0.0-3954d99.1.x86_64.rpm"
-  sha256 "473d89b1c3ed4d3d05db79709a1570aff900ffa7c0b352a0693322f620d221b1"
+  url "https://github.com/masahide/assumer/releases/download/v1.0.0/darwin-amd64.tar.gz"
+  sha256 "f6da0af345c60c5d999c51a77eb6733d26a0ade385cd1b4540f5ca35267de1fd"
   license ""
 
   # depends_on "cmake" => :build
